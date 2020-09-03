02932153a3ef0131abd41f8b131d5ba269f83d71
MaximeArn <maxime-arnould@outlook.fr>
2020-09-03 22:09:58 +0200

    create http server and connect and disconnect event

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const PORT = process.env.PORT || 3000 ;
+
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) =>{
+    res.sendFile( __dirname + '/public/html/')
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+      console.log('user disconnected');
+    });
+});
+
+http.listen(PORT);
