7c9915b62e25cc948c6e5f069f1686a464f89cf8
MaximeArn <maxime-arnould@outlook.fr>
2020-09-05 00:01:40 +0200

    different display for received messages and for sent messages

--- app.js
+++ app.js
@@ -4,21 +4,12 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 
-// const session = require('express-session')
-
-
 const PORT = process.env.PORT || 3000 ;
 
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
 app.use(express.static('public'));
-
-// app.use(session({
-//     secret: 'keyboard cat',
-//     saveUninitialized: true,
-//     resave: false,
-// }));
 
 app.use(express.urlencoded({extended : true}));
 
@@ -34,22 +25,33 @@ app.post('/addUsername', (req, res) =>{
 //executed for each user who connects to the server 
 io.on('connection', (socket) => {
     //create an "chat message" type event for every users that say user connected
-    io.emit('chat message', 'user connected');
+    io.emit('chat message', {
+        msg : 'user connected',
+        position : 'center',
+    });
     //executed for each user who disconnects to the server 
     socket.on('disconnect', () => {
         //create an "chat message" type event for every users that say user disconnected
-        io.emit('chat message', 'user disconnected');    
+        io.emit('chat message', {
+            msg : 'user disconnected',
+            position : 'center',
+        });    
     });
     //executed for each form submit 
     socket.on('chat message', (msg,) => {
-        console.log('send to other people !!');
-        io.emit('chat message', socket.username + " : " + msg);
+        io.emit('chat message', {
+            msg,
+            username : socket.username,
+        });
     });
     //custom event 
     socket.on('createUser', (username) =>{
-        console.log('username : ' + username);
+        console.log('new user : ' + username);
         //create a property username for each socket(user)
         socket.username = username;                
+    });
+    socket.on('userIsTyping', () =>{
+        console.log(`${socket.username} is typing`);
     });
 });
 
--- public/js/index.js
+++ public/js/index.js
@@ -4,40 +4,62 @@ const chat = {
         chat.messageForm = document.getElementById('messageForm');
         chat.usernameForm = document.getElementById('usernameForm');
         chat.usernameModal = document.getElementById('usernameModal');
+        chat.messageArea = document.getElementById('messageArea');
     },
 
     addEventListeners : () =>{
         chat.messageForm.addEventListener('submit', chat.createNewMessage);
         chat.usernameForm.addEventListener('submit', chat.createNewUser)
+        chat.messageArea.addEventListener('keydown', chat.userIsWritting)
+    },
+
+    userIsWritting : (event) =>{
+        chat.socket.emit('userIsTyping');
     },
 
     createNewMessage : async (event) =>{
         event.preventDefault();
-        const messageArea = document.getElementById('messageArea');
-        chat.socket.emit('chat message', messageArea.value, chat.socket.username);
-        console.log('new message !!');
+        //emit an event that will be recepted on server side
+        chat.socket.emit('chat message', chat.messageArea.value);
     },
 
     createNewUser : (event) =>{
         event.preventDefault();
 
         username = document.getElementById('usernameField').value;
-        console.log(username);
         //hide the modal
         chat.usernameModal.style.display = "none";
         //emit a custom event
         chat.socket.emit('createUser', username);
+        chat.socket.username = username;
     },
 
     displayMessage : () =>{
-        chat.socket.on('chat message', (msg) => {
+        chat.socket.on('chat message', (data) => {
             const messagesZone = document.getElementById('messages');
-            const newMessage = document.createElement('li');
-            newMessage.textContent = msg;
+            const newMessage = document.getElementById('messageTemplate').content.cloneNode(true);
+            const messageContent = newMessage.querySelector('.messageContent');
+            messageContent.textContent = data.msg;
+            //if an username is specified add a label that contains this username
+            if (data.username){
+                //if the message is from you add a class to display it on the right
+                if (data.username === chat.socket.username){
+                    data.position = 'right';
+                }
+                //construction of the label
+                const messageAuthor = newMessage.querySelector('.messageAuthor');
+                messageAuthor.classList.add('messageAuthorStyle');
+                messageAuthor.textContent = data.username;
+            }
+            //if a position is specified add a class 
+            if(data.position){
+                const message =newMessage.querySelector('.message');
+                message.classList.add(data.position);
+            };
             messagesZone.appendChild(newMessage);
         });
     },
-
+ 
     init : () =>{
         chat.socket = io();
         chat.getElementsFromDOM();
